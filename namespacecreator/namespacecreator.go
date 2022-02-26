e1388482f1f509fa347031f530e2802202ee26ab
david <david.cosc@gmx.net>
2022-02-26 22:02:32 +0000

    added verbose prints, and fixed chroot and namespaces except pid

--- namespacecreator/namespacecreator.go
+++ namespacecreator/namespacecreator.go
@@ -1,8 +1,9 @@
 package namespacecreator
 
 import (
+	"fmt"
 	"os"
-	"os/exec"
+	//"os/exec"
 	"syscall"
 )
 
@@ -13,24 +14,45 @@ type NamespaceCreator interface {
 type NamespaceCreatorImpl struct{}
 
 func (namespaceCreatorImpl *NamespaceCreatorImpl) CreateNamespaces() (int, string, error) {
-	err := syscall.Unshare(syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS)
+	// there is no point in unsharing the  PID namespace without forking
+	// unsharing the PID namespace does not move the calling process to the new PID namespace
+	// but instead moves the next child process
+	// to make full use of the PID namespace clone with NEWNS clone and unshare flag
+	fmt.Println("Creating namespaces")
+	err := syscall.Unshare(syscall.CLONE_NEWUTS | syscall.CLONE_NEWNS | syscall.CLONE_NEWPID)
 	if err != nil {
 		return -1, "", err
 	}
-	// err = syscall.Mount("proc", "/proc", "proc", 0, "")
-	if err != nil {
-		return -1, "", err
-	}
+	fmt.Println("Setting hostname")
 	err = syscall.Sethostname([]byte("container"))
+	if err != nil {
+		return -1, "", err
+	}
+	fmt.Println("Chrooting")
+	err = syscall.Chroot("/root/container")
+	if err != nil {
+		return -1, "", err
+	}
+	fmt.Println("Changing working directory")
+	err = syscall.Chdir("/")
+	if err != nil {
+		return -1, "", err
+	}
+	fmt.Println("Creating /proc dir if not exist")
+	if _, err := os.Stat("/proc"); os.IsNotExist(err) {
+		os.Mkdir("/proc", os.ModeDir)
+	}
+	fmt.Println("Mounting proc")
+	err = syscall.Mount("proc", "/proc", "proc", 0, "")
+	if err != nil {
+		return -1, "", err
+	}
+	fmt.Println("Cleanup mounts")
+	err = syscall.Unmount("/proc", 0)
 	if err != nil {
 		return -1, "", err
 	}
 	hostname, err := os.Hostname()
 	pid := os.Getpid()
-	cmd := exec.Command("/lsns")
-	cmd.Stdout = os.Stdout
-	cmd.Stdin = os.Stdin
-	cmd.Stderr = os.Stderr
-	cmd.Run()
 	return pid, hostname, err
 }
